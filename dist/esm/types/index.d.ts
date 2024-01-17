3c6227ea4859aa52995e1fa12e47c87059044ed1
Aditya <adityaraj@divami.com>
2024-01-17 13:00:17 +0530

    push 2

---
+++ dist/esm/types/index.d.ts
@@ -0,0 +1,1 @@
+export { default as SelectParent } from './components/Dropdown/';
---
+++ dist/index.d.ts
@@ -0,0 +1,23 @@
+import React from 'react';
+import { SelectProps } from '@mui/material/Select';
+
+interface CustomSelectProps extends SelectProps {
+    minWidth?: string | any;
+    options: any[];
+    backgroundColor?: string;
+    selectOptionsColor?: string;
+    selectOptionsSize?: string;
+    labelColor?: string;
+    labelSize?: string;
+    borderRadius?: string;
+    helperTextStyle?: any;
+    helperText?: string;
+    isRequired?: boolean;
+    isvalid?: boolean;
+    border?: string;
+    props?: any;
+    checkBox?: boolean;
+}
+declare const SelectParent: React.FC<CustomSelectProps>;
+
+export { SelectParent };
